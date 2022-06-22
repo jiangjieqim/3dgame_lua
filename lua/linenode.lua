e3ffc54923937bdb621b30acc19a35776f6cf9e5
jiangjieqim <jiangjieqim@hotmail.com>
2022-06-23 00:47:32 +0800

    LineCircle圆线组件

--- lua/linenode.lua
+++ lua/linenode.lua
@@ -79,3 +79,44 @@ end
 function LineNode:setRenderList(_list)
 	self._renderlist = _list;
 end
+--绘制圆
+LineCircle = {
+}
+LineCircle.__index = LineCircle;
+-- cnt 分割数,默认是8
+function LineCircle:new(x0,y0,z0,r,cnt)
+	local s = {};
+	setmetatable(s, LineCircle);
+	-- for(var times=0; times<60; times++) {
+	-- 	var hudu = (2*Math.PI / 360)* 6 * times;
+	-- 	var X = a + Math.sin(hudu) * r;
+	-- 	var Y = b - Math.cos(hudu) * r    //  注意此处是“-”号，因为我们要得到的Y是相对于（0,0）而言的。
+	-- }
+	cnt = cnt or 8;
+	local _l1 = LineNode:new(cnt + 1);
+	local a = 360 / cnt;
+	r = r or 1;
+	x0 = (x0 or 0);
+	z0 = z0 or 0;
+	local sx,sz;
+	for times = 0,cnt-1,1 do
+		local  hudu = (2*math.pi / 360)*a*times;
+		local x = x0 + math.sin(hudu)*r;
+		local z = z0 - math.cos(hudu)*r;
+		-- print(times,x,z);
+		if(times == 0) then
+			sx = x
+			sz = z;
+		end
+		_l1:push(x,y0,z);
+	end
+	_l1:push(sx,y0,sz);
+	_l1:graphics_end();
+	core.add(_l1);
+	s.line = _l1;
+	return s;
+end
+function LineCircle:dispose()
+	self.line:dispose();
+	func_clearTableItem(self);
+end
